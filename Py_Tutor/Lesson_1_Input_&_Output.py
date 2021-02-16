740750f9390b33ff38b1277997307267dde3f376
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-02-16 23:15:07 +0300

    Create Lesson_1_Input_&_Output.py

---
+++ Py_Tutor/Lesson_1_Input_&_Output.py
@@ -0,0 +1,30 @@
+#1 Summ of three numbers
+# Эта программа считывает три числа и выводит их сумму:
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a + b + c)
+
+
+#2 Area of the square angle triangle 
+# Числа b и h можно считывать так:
+b = int(input())
+h = int(input())
+# Выводите результат через print()
+S = 1/2 * b * h
+print(S)
+
+#3 Apples
+# Число n можно считать так:
+n = int(input())
+k = int(input())
+
+# Выводите результат через print()
+print(k // n)
+print (k % n)
+
+# Пример на деление, вычисление частного и остатка:
+#print(63 / 5)
+#print(63 // 5)
+#print(63 % 5)
