73eea8de4137fdc333566649ffd713576e8da475
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-02-16 23:20:11 +0300

    Update Lesson_1_Input_&_Output.py

--- Py_Tutor/Lesson_1_Input_&_Output.py
+++ Py_Tutor/Lesson_1_Input_&_Output.py
@@ -1,4 +1,4 @@
-#1 Summ of three numbers
+# Problem 1 Summ of three numbers----------------------
 # Эта программа считывает три числа и выводит их сумму:
 a = int(input())
 b = int(input())
@@ -7,7 +7,7 @@ c = int(input())
 print(a + b + c)
 
 
-#2 Area of the square angle triangle 
+# Problem 2 Area of the square angle triangle----------
 # Числа b и h можно считывать так:
 b = int(input())
 h = int(input())
@@ -15,7 +15,7 @@ h = int(input())
 S = 1/2 * b * h
 print(S)
 
-#3 Apples
+# Problem 3 Apples --------------------------------------
 # Число n можно считать так:
 n = int(input())
 k = int(input())
@@ -28,3 +28,28 @@ print (k % n)
 #print(63 / 5)
 #print(63 // 5)
 #print(63 % 5)
+
+#------------------ Problem 4 ----------------------------
+n = int(input())
+# (n % 1440) - кол-во минут в последних сутках
+print ((n % 1440) // 60)
+print ((n % 1440) % 60)
+
+# ---------------- Problem 5 -----------------------------
+s = input()
+print('Hello, ' + s + '!')
+
+# ---------------- Problem 6 -----------------------------
+n = int(input())
+print ('The next number for the number ' + str(n) + ' is ' + str(n+1) )
+print ('The previous number for the number ' + str(n) + ' is ' + str(n-1))
+
+# ---------------- Problem 7 -----------------------------
+n1, n2, n3 = int(input()), int(input()), int(input())
+print ((n1//2 + n2//2 + n3//2) + (n1%2 + n2%2 + n3%2))
+
+# ---------------- Problem 8 -----------------------------
+a, b, l, N = int(input()), int(input()), int(input()), int(input())
+print (2*l + (2*N-2)*(b + a) + a )
+
+
