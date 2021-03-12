07b1844eec6252d373aa79e772cdb67b113542d8
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-03-12 23:06:49 +0300

    Create Least_Squares_vs_RANSAC_vs_PCA.py

---
+++ Bootcamp/Least_Squares_vs_RANSAC_vs_PCA.py
@@ -0,0 +1,7 @@
+# https://tproger.ru/problems/zadacha-na-sobesedovanii-provesti-prjamuju-cherez-nabor-tochek/?utm_referrer=https%3A%2F%2Fzen.yandex.com&utm_campaign=dbr
+
+# Least_Squares
+
+# RANSAC
+
+# PCA
