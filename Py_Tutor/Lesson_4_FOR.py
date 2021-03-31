8d193a6b11b072f63dff688546c0fe828120fec5
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-03-31 06:38:45 +0300

    Update Lesson_4_FOR.py

--- Py_Tutor/Lesson_4_FOR.py
+++ Py_Tutor/Lesson_4_FOR.py
@@ -105,4 +105,32 @@ for i in range(1, n + 1):
         print(j, sep='', end='')
     print()
 
+# 11 Потерянная карточка
+
+Sk, Sg = 0, 0
+N = int(input())
+for i in range (1, N):
+    # задачу будем решать через контрольную сумму:    
+    Sk += int(input()) #Sk - сумма карточек
+    Sg += i          #Sg - cумма должна быть без N
+print (Sg - Sk + N)
+
+# решение разработчиков без доп переменной:
+n = int(input())
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+# можно доказать формулу:
+# sum == n * (n + 1) // 2
+# но мы посчитаем это значение циклом
+for i in range(n - 1):
+    sum -= int(input())
+print(sum)
+
+
+
+
+
+
+
 
