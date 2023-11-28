909dabf09e909f5f6d229c20787e4e10810b3aef
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2023-11-28 19:13:13 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -6,7 +6,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
 from pathlib import Path
 
 data_folder = Path("C:/Users/andrey.pimenov/Desktop/Exp/1")
@@ -14,15 +13,11 @@ data_folder = Path("C:/Users/andrey.pimenov/Desktop/Exp/1")
 # 4 5 6 - the relevant for Zirconium,
 # 7 - additional for Titanium
 
-file = "0000"
-string = "SN-2023-11-22-13-27-36-00000" + file + ".csv"
-file_to_open = data_folder / string
-file_to_save = data_folder / "biuld.txt"
-
 x_list = []
 y_list = []
 
-def parser_func():
+
+def parser_func(file_to_open):
     with open(file_to_open, 'r') as f:
         for i in range(9): # тут пропускаем мета-данные
             f.readline()
@@ -36,38 +31,54 @@ def parser_func():
             y = float(line[(boarder+1):])
             x_list.append(x)
             y_list.append(y)
-   
+
 def save_data_func(both_list_flag): # if 0 = init, if 1 = add
+    data_t = [str()] * 2048
     with open(file_to_save, 'r+') as f:
         if both_list_flag == 0:
             for i in range(len(x_list)):
                 x = x_list[i]
                 f.write(str(x))
                 f.write('\n')
+
         elif both_list_flag == 1:
             for i in range(len(y_list)):
                 y = y_list[i]
                 line = f.readline()
-                print (line)
-                #index = f.tell()
-                #print(index)
-                print(line)
-                #f.seek(index)
-                #output = line.replace('\n', '') + str(y) + '\n'
+
                 output = line.replace('\n', (" " + str(y) + '\n'))
-                print (output)
-                #str.replace(old, new[, count])
+                # print(output)
 
-                #index = f.tell()
-                #print(index)
-                #print(line)
-                #f.seek(index)
-                f.write(output)
+                data_t[i] = output
+                print(data_t[i])
+                data_t.append(output)
 
-parser_func()
+    with open(file_to_save, 'r+') as f:
+        for i in range(len(x_list)):
+            f.write(data_t[i])
 
+# Перестроить логику работы программы:
+# 1 - Берем один файл и записываем X
+# 2 - Потом в потоке берем в цикле каждый файл
+# 3 -
+
+# Парсим каждый фвйл и сохраняем X и Y для 1-го графика, а потом от каждого файла берем Y
+#
+
+# Шаг 1 - выбераем файл:
+file = "0000"
+string = "SN-2023-11-22-13-27-36-00000" + file + ".csv"
+file_to_open = data_folder / string
+file_to_save = data_folder / "biuld.txt"
+
+# Шаг 2 - парсим файл file = "0000":
+parser_func(file_to_open)
+
+# Шаг 3 - сохраняем этот файл с X и с Y:
 save_data_func(0)
-#print(x_list)
-print(y_list)
+
+print ("Сделал X")
+
 save_data_func(1)
 
+print ("Cделал Y")
